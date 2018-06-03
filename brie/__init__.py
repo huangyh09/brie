8dfab3ee2e7f8ccb76b564abe10b385dc0700b91
Yuanhua Huang <huangyh094@gmail.com>
2018-06-03 21:43:01 +0100

    upgrade to v0.1.5

--- brie/__init__.py
+++ brie/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 # import pyximport; pyximport.install()
 # from .utils.fasta import *
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
     
-reqs = ['numpy', 'pysam']
+reqs = ['numpy>=1.9.0', 'pysam>=0.9.0']
 
 setup(
     name='brie',
