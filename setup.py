42873ade2ed4e11288c7626182d6ef732aa1bd09
Aaron McGeever <aaron.mcgeever@czbiohub.org>
2019-01-10 18:07:56 -0800

    Fix issue with deps when installing.
    Currently there is a top level `import brie`.
    This causes the deps to be required before install is called.
    When doing either `python setup.py install` or `pip install brie`
    and error occurs if pysam and numpy are not already installed.

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-import brie
 
 here = path.abspath(path.dirname(__file__))
+
+# Set __version__ for the project.
+exec(open("./brie/version.py").read())
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-    
+
 reqs = ['numpy>=1.9.0', 'pysam>=0.9.0']
 
 setup(
@@ -24,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=brie.__version__,
+    version=__version__,
 
     description='BRIE: Bayesian regression for isoform estimate',
     long_description=long_description,
@@ -40,7 +42,7 @@ setup(
     license='Apache-2.0',
 
     # What does your project relate to?
-    keywords=['splicing isoform estimate', 'Bayesian regression', 
+    keywords=['splicing isoform estimate', 'Bayesian regression',
               'single cell RNA-seq', 'Markov chain Monte Carlo'],
 
     # You can just specify the packages manually here if your project is
@@ -48,20 +50,20 @@ setup(
     packages=find_packages(),
 
     entry_points={
-          'console_scripts': [
-              'brie = brie.brie:main',
-              'brie-diff = brie.brie_diff:main',
-              ],
-          }, 
+        'console_scripts': [
+            'brie = brie.brie:main',
+            'brie-diff = brie.brie_diff:main',
+        ],
+    },
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    
+
     install_requires=reqs,
 
-    py_modules = ['brie']
+    py_modules=['brie']
 
     # buid the distribution: python setup.py sdist
     # upload to pypi: twine upload dist/...
